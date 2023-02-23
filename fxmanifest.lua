48921396ffe87000cfbb433cbcd312a105eb6cbf
Viper <93407500+V1perino@users.noreply.github.com>
2023-02-23 18:49:46 +0100

    1.0

---
+++ fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'adamant'
+game 'gta5'
+
+replace_level_meta 'gta5'
+
+files {
+    'gta5.meta',
+    'trains.xml',
+	'traintracks.xml',
+	'trains1.dat',
+	'trains2.dat',
+	'trains3.dat',
+	'trains4.dat',
+	'trains5.dat',
+	'trains6.dat',
+	'trains7.dat',
+	'trains8.dat',
+	'trains9.dat',
+	'trains10.dat',
+	'trains11.dat',
+	'trains12.dat',
+}
+
+client_scripts {
+	'train.lua',
+}
+
+data_file 'TRAINTRACK_FILE' 'trains.xml'
+data_file 'TRAINTRACK_FILE' 'traintracks.xml'
+data_file 'TEXTFILE_METAFILE' 'trains1.dat'
+data_file 'TEXTFILE_METAFILE' 'trains2.dat'
+data_file 'TEXTFILE_METAFILE' 'trains3.dat'
+data_file 'TEXTFILE_METAFILE' 'trains4.dat'
+data_file 'TEXTFILE_METAFILE' 'trains5.dat'
+data_file 'TEXTFILE_METAFILE' 'trains6.dat'
+data_file 'TEXTFILE_METAFILE' 'trains7.dat'
+data_file 'TEXTFILE_METAFILE' 'trains8.dat'
+data_file 'TEXTFILE_METAFILE' 'trains9.dat'
+data_file 'TEXTFILE_METAFILE' 'trains10.dat'
+data_file 'TEXTFILE_METAFILE' 'trains11.dat'
+data_file 'TEXTFILE_METAFILE' 'trains12.dat'
